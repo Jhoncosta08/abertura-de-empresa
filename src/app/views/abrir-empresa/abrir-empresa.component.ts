89ef5b3cd6dd9c089b363f6eec096c1017f8176f
jhonatan costa <jhonatan.s.costa97@gmail.com>
2025-03-19 21:41:53 -0300

    feat(app): added the project theme and logo

---
+++ src/app/views/abrir-empresa/abrir-empresa.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-abrir-empresa',
+  imports: [],
+  templateUrl: './abrir-empresa.component.html',
+  styleUrl: './abrir-empresa.component.scss'
+})
+export class AbrirEmpresaComponent {
+
+}
