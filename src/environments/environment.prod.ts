76b86896d0015a841d0e43112962973ce8f0c918
jhonatan costa <jhonatan.s.costa97@gmail.com>
2025-03-20 00:11:32 -0300

    feat(firebase): added the firebase to the project

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  production: true,
+  apiKey: "AIzaSyBFnXrrcvuXksNiewGIhXS0zQTuzjIJt8Q",
+  authDomain: "abertura-de-empresa-app.firebaseapp.com",
+  projectId: "abertura-de-empresa-app",
+  storageBucket: "abertura-de-empresa-app.firebasestorage.app",
+  messagingSenderId: "573374564545",
+  appId: "1:573374564545:web:68e7ba487934688df6ac40"
+};
