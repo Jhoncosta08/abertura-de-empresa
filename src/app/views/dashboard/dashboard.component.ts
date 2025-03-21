d5733ad8f7ab6d8600a74b21e94c0e0fdf186344
jhonatan costa <jhonatan.s.costa97@gmail.com>
2025-03-21 02:00:43 -0300

    feat(dashboard): start working in dashboard page

--- src/app/views/dashboard/dashboard.component.ts
+++ src/app/views/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {EmpresaService} from '../../services/empresa/empresa.service';
+import {SolicitacaoInterface} from '../../interfaces/solicitacao.interfacec';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,4 +8,15 @@ import { Component } from '@angular/core';
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
-export class DashboardComponent {}
+export class DashboardComponent {
+  empresas: SolicitacaoInterface[] = [];
+
+  constructor(private empresaService: EmpresaService) {
+    void this.getAllEmpresa();
+  }
+
+  async getAllEmpresa(): Promise<void> {
+    this.empresas = await this.empresaService.getEmpresas();
+    console.log('Empresas: ', this.empresas);
+  }
+}
